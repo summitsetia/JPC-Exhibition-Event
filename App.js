0b0c0393e883adb2f9d9ea5364813b9eb3809d94
sumitsetia <summitsetia@gmail.com>
2023-07-26 00:29:55 +1200

    Navigation Container

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import description from "./description";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <ScreenStack.Navigator>
+        <Stack.Screen name="Info" component={description}/>
+      </ScreenStack.Navigator>
+      <View style={styles.container}>
+        <View style={styles.videoContainer}>
+          <Video
+            source={require('./videos/background.mp4')}
+            style={styles.backgroundVideo}
+            resizeMode={ResizeMode.COVER}
+            shouldPlay
+            isLooping 
+          />
+        </View>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>JPC EXHIBITION EVENT</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
+  header: {
+    backgroundColor: 'white',
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    borderRadius: 100,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 50, 
+    zIndex: 1, 
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  videoContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+    zIndex: 0, 
+  },
+  backgroundVideo: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+
+  
 });
